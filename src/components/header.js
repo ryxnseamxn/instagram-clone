7079cdd83169d77a0dcc502614d477171f2f3e27
Ryan Seaman <ryan.l.seaman@gmail.com>
2024-09-10 21:53:33 -0500

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+export default function Header() {
+    return (
+        <nav>
+            <li>Home</li>
+            <li>Profile</li>
+            <li>Sign Out</li>
+        </nav>
+    )
+}
