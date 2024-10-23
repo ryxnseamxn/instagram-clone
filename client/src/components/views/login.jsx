e4a42aa5fc448d93e37c9813b9891835b4466a37
Ryan Seaman <ryan.l.seaman@gmail.com>
2024-10-22 19:46:00 -0500

    added login redirect

--- client/src/components/views/login.jsx
+++ client/src/components/views/login.jsx
@@ -1,18 +1,57 @@
 import React, { useEffect, useState } from 'react';
-let Login = () => {
+import { Navigate } from 'react-router-dom'; 
+
+const Login = () => {
+    const [loggedIn, setLoggedIn] = useState(false); 
+    const [username, setUsername] = useState(''); 
+    const [password, setPassword] = useState(''); 
+
+    if(loggedIn){
+        return <Navigate to="/profile"/>
+    }    
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const response = await fetch('http://localhost:8000/login', {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username, 
+                    password 
+                })
+            })
+            if(response.ok){
+                setLoggedIn(true); 
+            }else{
+                alert('Login failed!');
+                window.location.reload();
+            }
+        }catch(err){
+            console.error("Error logging in", err); 
+            alert('An error has occured'); 
+        }
+    }
+
     return (
-        <form action="http://localhost:8000/login" method="post">
+        <form onSubmit={ handleSubmit }>
             <label htmlFor='username'>Username</label>
             <input
-            name="username"
             id="username"
             type="text"
+            value={ username }
+            onChange={(e) => setUsername(e.target.value)}
+            required
             />
             <label htmlFor='password'>Password</label>
             <input 
-            name="password"
             id="password"
             type="text"
+            password = { password }
+            onChange={(e) => setPassword(e.target.value)}
+            required
             />
             <input type="submit" value="Login"/>
         </form>
