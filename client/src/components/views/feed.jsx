23d77e606a3ced56c886aa558cf360e8b7d8190b
Ryan Seaman <ryan.l.seaman@gmail.com>
2024-11-03 22:23:58 -0600

    added feed view

--- client/src/components/views/feed.jsx
+++ client/src/components/views/feed.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Feed = () => {
-    
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await fetch(`http://localhost:8000/feed`, {
+                    credentials: 'include'
+                })
+                if (!response.ok) {
+                    throw new Error('Network response was not OK');
+                }
+                const data = await response.json();
+                console.log(data.posts); 
+                setPosts(data.posts);
+            } catch (err) {
+                console.log('Error fetching feed', err);
+            }
+        };
+
+        fetchPosts();
+    }, []);
+    return (
+        <div>
+            <h1>Home</h1>
+            {posts.length > 0 ?
+                posts.map((post, index) => (
+                    <div key={index}>
+                        <h2>{post.Username}</h2>
+                        <h2>{post.Caption}</h2>
+                        <img
+                            src={`http://localhost:8000/${post.Image}`}
+                            alt={`Post by ${post.Username}`}
+                            style={{ maxWidth: '300px', maxHeight: '300px' }}
+                        />
+                    </div>
+                )) : (
+                    <p>No posts found.</p>
+                )
+            }
+        </div>
+    )
 }
 
 export default Feed; 
